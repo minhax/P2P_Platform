f1563218dccbc46d44acc1fd425d2c971d9bcad4
paull <paul.lesur@free.fr>
2018-10-18 14:00:03 +0200

    [DATA] Classes communes : classes concernant les comptes utilisateurs

---
+++ src/com/lo23/common/user/User.java
@@ -0,0 +1,36 @@
+package com.lo23.common.user;
+
+import java.util.UUID;
+
+/**
+ * Classe User qui définie un utilisateur selon son UUID et son login.
+ */
+public class User {
+    /**
+     * Identifiant unique de l'utilisateur.
+     */
+    private UUID id;
+
+    /**
+     * Login utilisé lors de la connexion de l'utilisateur.
+     */
+    private String login;
+
+
+    /**
+     * Constructeur de User
+     * @param login Login du User
+     */
+    public User(String login) {
+        this.login = login;
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
---
+++ src/com/lo23/common/user/UserIdentity.java
@@ -0,0 +1,62 @@
+package com.lo23.common.user;
+
+/**
+ * Classe qui définie un utilisateur et ses informations de base.
+ */
+public class UserIdentity extends User {
+
+    /**
+     * Prénom de l'utilisateur
+     */
+    private String firstName;
+
+    /**
+     * Nom de famille de l'utilisateur.
+     */
+    private String lastName;
+
+    /**
+     * Age de l'utilisateur.
+     */
+    private int age;
+
+
+    /**
+     * Constructeur de UserIdentity
+     *
+     * @param login Login du User
+     * @param firstName Prénom de l'utilisateur
+     * @param lastName Nom de famille de l'utilisateur
+     * @param age Age de l'utilisateur
+     */
+    public UserIdentity(String login, String firstName, String lastName, int age) {
+        super(login);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
