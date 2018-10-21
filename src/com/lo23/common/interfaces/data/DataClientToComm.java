bfe80e21145b72377bb88ef63c1439eef2b4a1aa
Eva Terzago <eva.terzago@etu.utc.fr>
2018-10-21 15:11:33 +0200

    [data] API data (wip)

--- src/com/lo23/common/interfaces/data/DataClientToComm.java
+++ src/com/lo23/common/interfaces/data/DataClientToComm.java
@@ -1,4 +1,81 @@
 package com.lo23.common.interfaces.data;
 
-public interface DataClientToComm {
+import com.lo23.common.Comment;
+import com.lo23.common.Rating;
+import com.lo23.common.filehandler.FileHandler;
+import com.lo23.common.filehandler.FileHandlerInfos;
+import com.lo23.common.user.User;
+import com.lo23.common.user.UserIdentity;
+import com.lo23.common.user.UserStats;
+
+import java.util.List;
+
+public interface DataClientToComm
+{
+
+    /**
+     * Notifie Comm de l'ajout d'un commentaire sur un fichier
+     * @param comment commentaire
+     * @param file fichier commenté
+     */
+    public void sentFileChanges(Comment comment, FileHandler file);
+
+    /**
+     * Demande à Comm les sources du fichier que l'utilisateur
+     * veut télécharger
+     * @param file fichier à télécharger
+     * @param user utilisateur qui télécharge le fichier // TODO askip c'est pour dire au serveur de renvoyer les sources de fichiers à cet utilisateur
+     */
+    public void requestFileLocToServer(FileHandler file, UserIdentity user);
+
+    //public void connect(UserStats user, ServerInfo server); // TODO diag Envoyer demande de connexion
+    //public void requestLogout(User user, ServerInfo server); // TODO diag Envoyer demande de connexion
+
+    /**
+     * Envoie à Comm le descripteur de fichier contenant
+     * les changements apportés à ce fichier
+     * @param file nouveau descripteur de fichier qui remplacera l'ancien
+     */
+    public void sendFilesChanges(FileHandler file);
+
+    /**
+     * Envoie à Comm le descripteur d'utilisateur contenant
+     * les changement (autres que mdp) apportés à cet utilisateur
+     * @param user nouveau descripteur d'utilisateur qui remplacera l'ancien
+     */
+    public void sendUserChanges(UserIdentity user);
+
+    /**
+     * Envoie à Comm la note attribuée à un fichier
+     * @param rating note à comptabiliser
+     * @param file fichier sur lequel appliquer la note
+     */
+    public void sendFileChanges(Rating rating, FileHandler file);
+
+    /**
+     * Envoie à Comm le nouvel utilisateur source pour un fichier
+     * dans le cas où le fichier téléversé par cet utilisateur
+     * existe déjà et donc dans le cas où il faut ajouter cet
+     * utilisateur aux sources existantes pour ce fichier
+     * @param file fichier
+     * @param user nouvelle source
+     */
+    public void sendNewFileSource(FileHandler file, UserIdentity user);
+
+    /**
+     *
+     * @param user
+     * @param files
+     */
+    public void sendUpdates(UserIdentity user, List<FileHandlerInfos> files);
+
+    /**
+     * Envoie à Comm le fichier qu'un utilisateur souhaite
+     * rendre indisponible
+     * @param file fichier à rendre indisponible
+     * @param User utilisateur qui rend le fichier indisponible
+     */ // TODO pourquoi appeler ça sendFileChanges ?
+    public void sendFileChanges(FileHandler file, User User);
+
+
 }
--- src/com/lo23/common/interfaces/data/DataClientToIhm.java
+++ src/com/lo23/common/interfaces/data/DataClientToIhm.java
@@ -1,4 +1,59 @@
 package com.lo23.common.interfaces.data;
 
-public interface DataClientToIhm {
+import com.lo23.common.filehandler.FileHandler;
+import com.lo23.common.user.UserIdentity;
+
+import java.util.Vector;
+
+public interface DataClientToIhm
+{
+    /**
+     * Demande la mise à jour des fichiers disponibles
+     * à afficher pour l'utilisateur
+     * @param file nouveau fichier
+     * @param user utilisateur qui a téléversé
+     */
+    public void updateAvailableFiles(FileHandler file, UserIdentity user);
+
+
+    // TODO ces deux méthodes sont distinctes ? Ou la même ?
+    /**
+     * Demande à l'IHM de mettre à jour ses utilisateurs connectés
+     * suite à la connexion d'un utilisateur
+     * @param user utilisateur nouvellement connecté
+     * @param files fichiers dont il est la source
+     */
+    public void sendUpdatesUserConnected(UserIdentity user, Vector<FileHandler> files);
+
+    /**
+     * Demande à l'IHM de mettre à jour ses utilisateurs connectés
+     * suite à la déconnexion d'un utilisateur
+     * @param user utilisateur nouvellement déconnecté
+     * @param files fichiers dont il est la source
+     */
+    public void sendUpdatesUserDisconnected(UserIdentity user, Vector<FileHandler> files);
+
+    /**
+     * Demande à l'IHM de mettre à jour les sources d'un fichier
+     * suite à l'ajout d'une nouvelle source
+     * @param file fichier dont les sources seront mises à jour
+     * @param user nouvelle source
+     */
+    public void addFileSource(FileHandler file, UserIdentity user);
+
+    /**
+     * Demande à l'IHM de mettre à jour les sources d'un fichier
+     * @param users sources du fichier
+     * @return
+     */
+    public UserIdentity updateFileDownload(Vector<UserIdentity> users);
+
+    /**
+     * Envoie à l'IHM le pourcentage de complétion du téléchargement
+     * @param percentage pourcentage de complétion du téléchargement
+     */
+    public void showPercentageComplete(float percentage);
+
+
+
 }
