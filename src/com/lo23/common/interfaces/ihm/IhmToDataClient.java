7d9be4ab99af7122ed9dc084f1303dd49f9c1bb3
Héloïse Chevalier <heloise.chevalier@etu.utc.fr>
2018-10-23 18:54:39 +0200

    [IHM] interface wip

--- src/com/lo23/common/interfaces/ihm/IhmToDataClient.java
+++ src/com/lo23/common/interfaces/ihm/IhmToDataClient.java
@@ -1,4 +1,41 @@
 package com.lo23.common.interfaces.ihm;
 
+import com.lo23.common.filehandler.FileHandlerInfos;
+import com.lo23.common.user.User;
+import com.lo23.common.user.UserAccount;
+import com.lo23.common.user.UserIdentity;
+
+import java.util.List;
+
 public interface IhmToDataClient {
+
+    /**
+     *
+     * @return
+     */
+    public static List<FileHandlerInfos> getAvailableFiles()
+    {
+        return null;
+    }
+
+    public static List<FileHandlerInfos> getUploadedFiles()
+    {
+        return null;
+    }
+
+    public static List<UserIdentity> regreshConnectedUsers()
+    {
+        return null;
+    }
+
+    public static registerForm requestRegisterForm()
+    {
+        return null;
+    }
+
+    public static UserAccount requestAccountData(User user)
+    {
+        return null;
+    }
+
 }
