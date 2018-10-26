1567d4debad54706be39e0e6ec4d199856337c6c
Héloïse Chevalier <heloise.chevalier@etu.utc.fr>
2018-10-26 21:58:12 +0200

    [IHM] interfaces corrigées

--- src/com/lo23/common/interfaces/ihm/IhmToDataClient.java
+++ src/com/lo23/common/interfaces/ihm/IhmToDataClient.java
@@ -10,32 +10,59 @@ import java.util.List;
 public interface IhmToDataClient {
 
     /**
-     *
-     * @return
+     * Demande à Data les champs que l'utilisateur devra renseigner
+     * @return registerForm
      */
-    public static List<FileHandlerInfos> getAvailableFiles()
-    {
-        return null;
-    }
+    public registerForm requestRegisterForm();
 
-    public static List<FileHandlerInfos> getUploadedFiles()
-    {
-        return null;
-    }
+    /**
+     * IHM envoie les informations de l'utilisateur à Data pour la création de compte en local
+     * @param username nom d'utilisateur
+     * @param password mot de passe
+     * @param serverIP adresse du serveur
+     * @param userInfo informations de l'utilisateur
+     */
+    public void requestRegisterForm(String username, String password, String serverIP, String userInfo);
 
-    public static List<UserIdentity> regreshConnectedUsers()
-    {
-        return null;
-    }
+    /**
+     * IHM envoie les informations de l'utilisateur à Data pour les vérifications pour connexion
+     * @param username nom d'utilisateur
+     * @param password mot de passe
+     * @param serverIP adresse du serveur
+     * @param userInfo informations de l'utilisateur
+     */
+    public void checkAccount(String username, String password, String serverIP, String userInfo);
 
-    public static registerForm requestRegisterForm()
-    {
-        return null;
-    }
+    /**
+     * IHM transmet à Data la demande de déconnexion
+     */
+    public void logout();
 
-    public static UserAccount requestAccountData(User user)
-    {
-        return null;
-    }
+    /**
+     * IHM récupère les informations du compte pour l'exportation
+     * @param user informations du compte
+     * @return UserAccount
+     */
+    public UserAccount requestAccountData(User user);
+
+    /**
+     * IHM envoie les informations du compte pour l'importation
+     * @param user
+     */
+    public void storeDataAccount(UserAccount user);
+
+    /**
+     * IHM récupère les informations du compte pour la modification
+     * @return UserAccount
+     */
+    public UserAccount requestAccountInfo();
+
+    /**
+     * IHM envoie à Data les données du compte modifié
+     * @param user : nouvelles données d'utilisateur (UserAccount)
+     */
+    public void submitUserChanges(UserAccount user);
+
+
 
 }
