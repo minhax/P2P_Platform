d9d29c5674957ac88aa29028f7c17ced1ed5bf7c
paull <paul.lesur@free.fr>
2018-10-30 12:07:37 +0100

    [DATA] WIP Connexion utilisateur coté serveur : implémentation des classes ConnectionsManager et DirectoryUserFiles

---
+++ src/main/java/com/lo23/data/server/ConnectionsManager.java
@@ -0,0 +1,107 @@
+package com.lo23.data.server;
+
+import com.lo23.common.filehandler.FileHandlerInfos;
+import com.lo23.common.user.UserIdentity;
+
+import java.util.Set;
+import java.util.Vector;
+
+/**
+ * Permet de gérer les connections des utilisateurs ainsi que les fichiers proposés par ces utilisateurs.
+ */
+public class ConnectionsManager
+{
+    /**
+     * Vecteur des utilisateurs connectés
+     */
+    private Vector<UserIdentity> connectedUsers;
+    /**
+     * Annuaires des fichiers proposés par utilisateurs.
+     */
+    private DirectoryUserFiles directory;
+
+    /**
+     * Constructeur de gestionnaire de connexions.
+     */
+    public ConnectionsManager()
+    {
+        this.connectedUsers = new Vector<>();
+        this.directory = new DirectoryUserFiles();
+    }
+
+    /**
+     * Permet de connecter un utilisateur
+     * @param user Utilisateur à connecter
+     * @throws IllegalArgumentException Exception levée si le paramètre passé est mauvais
+     * @throws IllegalStateException Exception levée si l'utilisateur est déjà connecté
+     */
+    public void connectUser(UserIdentity user) throws IllegalArgumentException, IllegalStateException
+    {
+        // Si le paramètre passé est null
+        if (user == null)
+        {
+            throw new IllegalArgumentException("User should not be null");
+        }
+
+        // Si l'utilisateur est déjà connecté
+        if (this.connectedUsers.contains(user))
+        {
+            throw new IllegalStateException("User is already connected !");
+        }
+
+        // On connecte l'utilisateur
+        this.connectedUsers.add(user);
+    }
+
+    /**
+     * Permet de deconnecter un utilisateur
+     * @param user Utilisateur à deconnecter
+     * @throws IllegalArgumentException Exception levée si le paramètre passé est mauvais
+     * @throws IllegalStateException Exception levée si l'utilisateur n'est pas connecté
+     */
+    public void disconnectUser(UserIdentity user) throws IllegalArgumentException, IllegalStateException
+    {
+        // Si le paramètre passé est null
+        if (user == null)
+        {
+            throw new IllegalArgumentException("User should not be null");
+        }
+
+        // Si l'utilisateur n'est pas connecté
+        if (!this.connectedUsers.contains(user))
+        {
+            throw new IllegalStateException("User is not connected !");
+        }
+
+
+        try
+        {
+            // On deconnecte l'utilsateur
+            this.connectedUsers.remove(user);
+            this.directory.removeUser(user);
+        }
+        catch (IllegalArgumentException iae)
+        {
+            iae.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Retourne un vecteur des utilisateurs connectés
+     * @return Les utilisateur connectés
+     */
+    public Vector<UserIdentity> getConnectedUsers(){
+        return this.connectedUsers;
+    }
+
+    /**
+     * Permet d'obtenir les fichiers proposés
+     * @return Un ensemble contenant les fichiers proposés.
+     */
+    public Set<FileHandlerInfos> getProposedFiles(){
+        return this.directory.getProposedFiles();
+    }
+
+
+}
---
+++ src/main/java/com/lo23/data/server/DirectoryUserFiles.java
@@ -0,0 +1,191 @@
+package com.lo23.data.server;
+
+import com.lo23.common.filehandler.FileHandlerInfos;
+import com.lo23.common.user.UserIdentity;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Vector;
+
+/**
+ * Annuaire permettant de gérer les fichiers proposés par les utilisateurs.
+ */
+public class DirectoryUserFiles
+{
+    /**
+     * Map contenant les fichiers proposés par les utilisateurs.
+     */
+    private HashMap<UserIdentity, Vector<FileHandlerInfos>> userFiles;
+    /**
+     * Map contenant les utilisateurs qui proposent chaque fichier.
+     */
+    private HashMap<FileHandlerInfos, Vector<UserIdentity>> filesUser;
+
+    /**
+     * Constructeur qui instancie les deux HashMap permettant de gérer l'annuaire.
+     */
+    public DirectoryUserFiles()
+    {
+        this.userFiles = new HashMap<>();
+        this.filesUser = new HashMap<>();
+    }
+
+    /**
+     * Permet d'ajouter un couple UserIdentity, FileHandlerInfos à l'annuaire des fichiers proposés.
+     * @param user Utilisateur
+     * @param file Fichier
+     * @throws IllegalStateException Levée si fichier déjà proposé par cet utilisateur
+     * @throws IllegalArgumentException Levée si les paramètres passés sont mauvais
+     */
+    public void addProposedFile(UserIdentity user, FileHandlerInfos file) throws IllegalStateException, IllegalArgumentException
+    {
+        // On vérifie que les paramètres passés sont valides, sinon on lève une exception.
+        if (user == null)
+        {
+            throw new IllegalArgumentException("User should not be null !");
+        }
+        if (file == null)
+        {
+            throw new IllegalArgumentException("FileHandlerInfos should not be null !");
+        }
+
+        // On lève une exception si l'utilisateur propose déjà ce fichier
+        if (this.userFiles.get(user).contains(file) && this.filesUser.get(file).contains(user) ){
+            throw new IllegalStateException("This user already propose this file !");
+        }
+
+        // On vérifie si l'utilisateur propose déjà des fichiers, si ce n'est pas le cas on l'ajoute à la map
+        if (this.userFiles.get(user) == null)
+        {
+            Vector<FileHandlerInfos> v = new Vector<>();
+            v.add(file);
+            this.userFiles.put(user, v);
+        }
+        // Sinon on ajoute simplement le fichier
+        else
+        {
+            this.userFiles.get(user).add(file);
+        }
+
+
+        // On vérifie si le fichier est déjà proposé, si ce n'est pas le cas on l'ajoute à la map
+        if (this.filesUser.get(file) == null)
+        {
+            Vector<UserIdentity> v = new Vector<>();
+            v.add(user);
+            this.filesUser.put(file, v);
+        }
+        // Sinon on ajoute simplement le fichier
+        else
+        {
+            this.filesUser.get(file).add(user);
+        }
+    }
+
+
+    /**
+     * Permet d'enlever un couple UserIdentity FileHandlerInfos de l'annuaire
+     * @param user Utilisateur
+     * @param file Fichier
+     * @throws IllegalArgumentException Exception levée si mauvais arguements passés
+     */
+    public void removeProposedFile(UserIdentity user, FileHandlerInfos file) throws IllegalArgumentException
+    {
+        // On vérifie que les paramètres passés sont valides, sinon on lève une exception.
+        if (user == null)
+        {
+            throw new IllegalArgumentException("User should not be null !");
+        }
+        if (file == null)
+        {
+            throw new IllegalArgumentException("FileHandlerInfos should not be null !");
+        }
+
+        // On lève une exception si l'utilisateur propose déjà ce fichier, sinon on enlève les informations des HashMap
+        if (this.userFiles.get(user).contains(file) && this.filesUser.get(file).contains(user))
+        {
+            // On enlève le fichier
+            this.userFiles.get(user).remove(file);
+            this.filesUser.get(file).remove(user);
+
+            // Nettoyage des HashMap
+            if (this.getFilesProposedByUser(user).isEmpty())
+            {
+                this.userFiles.remove(user);
+            }
+
+            if (this.getUsersThatProposeFile(file).isEmpty())
+            {
+                this.filesUser.remove(user);
+            }
+
+        }
+        else
+        {
+            throw new IllegalArgumentException("This file is not proposed by this user !");
+        }
+    }
+
+    /**
+     * Permet de supprimer un utilisateur et tous ses fichiers de l'annuaire
+     * @param user Utilisateur à supprimer
+     */
+    public void removeUser(UserIdentity user) throws IllegalArgumentException
+    {
+        if (user == null)
+        {
+            throw new IllegalArgumentException("User should not be null");
+        }
+        if (this.userFiles.get(user) == null)
+        {
+            throw new IllegalArgumentException("User does not propose files...");
+        }
+
+        // Suppression de tous les fichiers de l'utilisateur
+        for (FileHandlerInfos f :
+                this.getFilesProposedByUser(user))
+        {
+            this.removeProposedFile(user, f);
+        }
+    }
+
+    /**
+     * Permet de récupérer les fichiers proposés par un utilisateur
+     * @param user Utilisateur
+     * @return Vector contenant les fichiers proposés par l'utilisateur passé en paramètre
+     * @throws NullPointerException Exception levée si l'utilisateur ne propose aucun fichier
+     */
+    public Vector<FileHandlerInfos> getFilesProposedByUser(UserIdentity user) throws NullPointerException
+    {
+        // Est ce que l'utilisateur propose des fichiers ?
+        if (this.userFiles.get(user) == null)
+        {
+            throw new NullPointerException("This user has no files !");
+        }
+        return this.userFiles.get(user);
+    }
+
+    /**
+     * Permet de récupérer les utilisateurs proposant un fichier
+     * @param file Fichier
+     * @return Vector contenant les utilisateurs proposant ce fichier
+     * @throws NullPointerException Exception levée si le fichier n'est proposé par aucun utilisateur
+     */
+    public Vector<UserIdentity> getUsersThatProposeFile(FileHandlerInfos file) throws NullPointerException
+    {
+        if (this.filesUser.get(file) == null)
+        {
+            throw new NullPointerException("This file is not available !");
+        }
+        return this.filesUser.get(file);
+    }
+
+    /**
+     * Permet de récupérer tous les fichiers proposés actuellement
+     * @return Un Set des fichiers proposés.
+     */
+    public Set<FileHandlerInfos> getProposedFiles()
+    {
+        return this.filesUser.keySet();
+    }
+}
