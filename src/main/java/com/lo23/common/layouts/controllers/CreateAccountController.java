78bfe1e98d48eb96c92882d7762f113244546de1
Alexandre Bech <alexandre.bech@etu.utc.fr>
2018-11-08 18:08:00 +0100

    [IHM] Modification appel API fenetre creation compte + lien avec fenêtre principal quand compte cree

--- src/main/java/com/lo23/common/layouts/controllers/CreateAccountController.java
+++ src/main/java/com/lo23/common/layouts/controllers/CreateAccountController.java
@@ -3,12 +3,18 @@ package com.lo23.common.layouts.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.lo23.common.interfaces.ihm.IhmToDataClient;
 import com.lo23.common.layouts.models.CreateAccountModel;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class CreateAccountController implements Initializable {
     @FXML
@@ -29,8 +35,23 @@ public class CreateAccountController implements Initializable {
 		@FXML
 	public void OnCreateAccountClicked()
 	{	// A decommenté et verifier les champs pendant l'integration
-		//IhmToDataClient.requestRegisterForm(loginTextField.getText(),passwordField.getText(),firstnameTextField.getText(),loginTextField.getText(),ageTextField.getText());
-		System.out.println(loginTextField.getText() +passwordField.getText()+firstnameTextField.getText()+loginTextField.getText()+ageTextField.getText());
+		/*IhmToDataClient api = new IhmToDataClientApi();
+		api.createAccount(loginTextField.getText(),passwordField.getText(),firstnameTextField.getText(),lastnameTextField.getText(),Integer.parseInt(ageTextField.getText()));
+		*/System.out.println(loginTextField.getText() +passwordField.getText()+firstnameTextField.getText()+lastnameTextField.getText()+ageTextField.getText());
+
+		try {
+			FXMLLoader fxmlloader = new FXMLLoader(getClass().getClassLoader().getResource("com/lo23/common/layouts/mainLayout.fxml"));
+			Parent root = fxmlloader.load();
+			Stage stage = new Stage();
+
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setOpacity(1);
+			stage.setTitle("Fenêtre principale");
+			stage.setScene(new Scene(root));
+			stage.showAndWait();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 
