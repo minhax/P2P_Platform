7a9e6ce429b7dcb2e79557e75682ce711ec451cc
Sevan <sevan@selectively.co>
2018-11-13 18:37:20 +0100

    [IHM] Ajout de la view fxml et du controller du profil user update

---
+++ src/main/java/com/lo23/common/layouts/controllers/updateProfileController.java
@@ -0,0 +1,48 @@
+package com.lo23.common.layouts.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class updateProfileController {
+
+    @FXML
+    private TextField loginUpdateField;
+
+    @FXML
+    private TextField passwordUpdateField;
+
+    @FXML
+    private TextField familynameUpdateField;
+
+    @FXML
+    private TextField nameUpdateField;
+
+    @FXML
+    private TextField birthdateUpdateField;
+
+    @FXML
+    private Button avatarUpdateButton;
+
+    @FXML
+    private Button validateUpdateButton;
+
+    @FXML
+    private Button previousButton;
+
+    @FXML
+    void Previous(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Validate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateAvatar(ActionEvent event) {
+
+    }
+
+}
