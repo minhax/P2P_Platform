6a798d3734b2568937c5c0ac66fad6665b64ef4a
Héloïse Chevalier <heloise.chevalier@etu.utc.fr>
2018-11-14 13:48:51 +0100

    [IHM] layout & controller contacts window (wip)

---
+++ src/main/java/com/lo23/common/layouts/controllers/ContactsController.java
@@ -0,0 +1,60 @@
+package com.lo23.common.layouts.controllers;
+
+import com.lo23.common.layouts.models.ContactsModel;
+import javafx.beans.property.ListProperty;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ContactsController {
+
+    @FXML
+    private AnchorPane contactsPane;
+
+    @FXML
+    private ListView<String> contactsListView;
+
+    @FXML
+    private Button refreshConnectedUsersButton;
+
+    private List<String> userList;
+
+    private ListProperty<String> userListProperty ;
+
+
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        //refreshConnectedUsers();
+        binding();
+        userListProperty.set(FXCollections.observableArrayList(userList));
+        // TODO Auto-generated method stub
+
+    }
+
+    @FXML
+    public void OnRefreshConnectedUsersClicked()
+    {
+        try
+        {
+            //refreshConnectedUsers();
+            //userListProperty.set(FXCollections.observableArrayList(userList));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void binding()
+    {
+        this.contactsListView.itemsProperty().bind(userListProperty);
+    }
+
+}
