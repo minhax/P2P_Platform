d03f9fbad4b80268d2df33acac90d57ecda60776
Minh Nguyen <minh.nguyen@etu.utc.fr>
2018-12-01 15:55:00 +0100

    fix

--- src/main/java/com/lo23/communication/CommunicationManager/CommunicationManager.java
+++ src/main/java/com/lo23/communication/CommunicationManager/CommunicationManager.java
@@ -12,43 +12,16 @@ public abstract class CommunicationManager {
 	public String getIp() {
 		return this.ip;
 	}
-
-/**
- * Retourne et affiche l'adresse IP sur le serveur UTC de la machine appelante
- *
- * @param
- * ... TODO FIX HARDCODE EQUALS
- * @return String IPadress
-**/
+	
+	/**
+	 * Retourne et affiche l'adresse IP sur le serveur UTC de la machine appelante
+	 *
+	 * @param ... TODO FIX HARDCODE EQUALS
+	 * @return String IPadress
+	 **/
 	public static String findIPadress() throws Exception {
 		
-		Enumeration<NetworkInterface> interfaces = null;
-		try {
-			interfaces = NetworkInterface.getNetworkInterfaces();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-		while (interfaces.hasMoreElements()) {
-			NetworkInterface networkInterface = interfaces.nextElement();
-			// drop inactive
-			try {
-				if (!networkInterface.isUp())
-					continue;
-			} catch (SocketException e) {
-				e.printStackTrace();
-			}
-			// smth we can explore
-			Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
-			while (addresses.hasMoreElements()) {
-				InetAddress addr = addresses.nextElement();
-				String ip = addr.getCanonicalHostName().toString();
-				if (ip.regionMatches(0, "172", 0, 3)) {
-					System.out.println("Ajout de l'adresse IP " + ip);
-					return ip;
-				} else
-					continue;
-			}
-		}
-		return null;
+		String ip = "192.168.0.19";
+		return ip;
 	}
 }
