552fd3fc7b5e282640bd12781865dfb303e53e15
Wassim Zerouati <wassim.zerouati@gmail.com>
2018-12-11 17:42:33 +0100

    [COMM] Fix Serializable Messages

--- src/main/java/com/lo23/communication/Messages/Message.java
+++ src/main/java/com/lo23/communication/Messages/Message.java
@@ -4,6 +4,7 @@ import com.lo23.common.interfaces.data.DataClientToComm;
 import java.io.Serializable;
 
 public abstract class Message implements Serializable {
+    private static final long serialVersionUID = 42L;
 	private int port;
 	public abstract void treatment();
 	public abstract boolean isToServ();
