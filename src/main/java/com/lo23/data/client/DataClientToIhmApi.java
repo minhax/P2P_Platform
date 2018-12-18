120f47df9a1853f0e3f4da744453a00142e78926
paull <paul.lesur@free.fr>
2018-12-18 18:48:36 +0100

    [DATA] Bugfix pour la recherche de fichier

--- src/main/java/com/lo23/data/client/DataClientToIhmApi.java
+++ src/main/java/com/lo23/data/client/DataClientToIhmApi.java
@@ -210,7 +210,7 @@ public class DataClientToIhmApi implements DataClientToIhm
                 returnedFiles.add(fhi);
             }
         }
-        return null;
+        return returnedFiles;
     }
 
     @Override
